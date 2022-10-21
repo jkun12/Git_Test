36813f74ec6389be6e1827c19bd19fbd728560a5
jkun <jkun12@163.com>
2022-10-22 01:56:39 +0800

    first commit

---
+++ src/main/java/com/djk/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.djk.git;
+
+/**
+ * projectName Git_Test
+ * packageName com.djk.git
+ *
+ * @author Jkun
+ * @version 1.0
+ * @Date 2022/10/22/022 1:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git ! ");
+    }
+}
