3fe531d64af2a23327f37b7d6e9ad8096e067910
jkun <jkun12@163.com>
2022-10-22 01:59:49 +0800

    second commit

--- src/main/java/com/djk/git/GitTest.java
+++ src/main/java/com/djk/git/GitTest.java
@@ -11,5 +11,6 @@ package com.djk.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git ! ");
+        System.out.println(" 第二代版本 Git ");
     }
 }
